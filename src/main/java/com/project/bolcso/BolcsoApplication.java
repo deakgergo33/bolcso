29a46436aa216c8ef2441e10f5d440e4d89785c6
mredly <mredly@konzorcia.hu>
2021-07-03 13:52:47 +0200

    Init Spring boot

---
+++ src/main/java/com/project/bolcso/BolcsoApplication.java
@@ -0,0 +1,13 @@
+package com.project.bolcso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolcsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolcsoApplication.class, args);
+	}
+
+}
