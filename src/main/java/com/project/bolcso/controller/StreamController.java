7b63f1852cc8d8c9c1f9e90cde4834c1af1212e9
mredly <mredly@konzorcia.hu>
2021-07-05 16:34:33 +0200

    Init camera handling

--- src/main/java/com/project/bolcso/controller/StreamController.java
+++ src/main/java/com/project/bolcso/controller/StreamController.java
@@ -1,15 +1,37 @@
 package com.project.bolcso.controller;
 
+import boofcv.io.webcamcapture.UtilWebcamCapture;
+import com.github.sarxos.webcam.Webcam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.image.BufferedImage;
 
 @Controller
 @RequestMapping(value = "/stream")
 public class StreamController {
 
-    @RequestMapping(method = RequestMethod.GET, value="/start")
-    public void start() {
+    private boolean isStreaming = false;
 
+    @RequestMapping(method = RequestMethod.GET, value="/start")
+    @ResponseBody
+    public void start() {
+        if (!isStreaming) {
+            isStreaming = true;
+            Webcam webcam = UtilWebcamCapture.openDefault(640, 480);
+            while (isStreaming) {
+                BufferedImage image = webcam.getImage();
+                //TODO: send to client
+            }
+            webcam.close();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="/stop")
+    @ResponseBody
+    public void stop() {
+        isStreaming = false;
     }
 }
