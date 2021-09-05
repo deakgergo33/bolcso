9328bdbde48c1bae458878da14cb994a05fea2b5
mredly <mredly@konzorcia.hu>
2021-09-05 14:37:49 +0200

    python server init

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 cd ..
-cp bolcso/server.py bolcso-py-server
+cp bolcso/server.py bolcso-py-server/venv/bin
 cp -r bolcso/templates/* bolcso-py-server/venv/bin/templates
 cp -r bolcso/static/js/* bolcso-py-server/venv/bin/static/js
 cp -r bolcso/static/css/* bolcso-py-server/venv/bin/static/css
