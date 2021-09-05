45425574183c4d2df94b9c78fc36a9828ea97c9b
mredly <mredly@konzorcia.hu>
2021-09-05 10:13:36 +0200

    python server init

---
+++ install.sh
@@ -0,0 +1,12 @@
+cd ..
+mkdir bolcso-py-server
+cd bolcso-py-server
+python3 -m venv venv
+. venv/bin/activate
+pip install Flask
+cd venv/Scripts
+mkdir static
+mkdir templates
+cd ..
+cd ..
+cd ..
---
+++ update.sh
@@ -0,0 +1,4 @@
+cp -r bolcso/templates/* bolcso-py-server/venv/Scripts/templates
+cp bolcso/server.py bolcso-py-server
+cp -r bolcso/static/js/* bolcso-py-server/venv/Scripts/static/js
+cp -r bolcso/static/css/* bolcso-py-server/venv/Scripts/static/css
